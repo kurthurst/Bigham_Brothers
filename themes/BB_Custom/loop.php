0ef6602241506b9c295a94b85434fb3d667de1f5
Kurt Hurst <kurthurst@Kurts-MacBook-Air.local>
2013-07-19 19:49:06 -0500

    fixed featured image

--- themes/BB_Custom/loop.php
+++ themes/BB_Custom/loop.php
@@ -34,9 +34,9 @@
 <?php } elseif(is_page()){?>
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 <div class="row"><div class="c12"><h2><?php the_title(); ?></h2>
-<?php if(have_post_thumbnail()){
-  the_post_thumbnail('large');
-} else {} ?>
+<?php if(has_post_thumbnail()) { 
+        the_post_thumbnail('large');
+      } else {} ?>
 </div></div>
 <div class="row clearfix">
   <div class="c8">
