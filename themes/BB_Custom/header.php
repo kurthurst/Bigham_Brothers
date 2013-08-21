ed228bd4b5c397f9c460c42a32d654166017ad0b
Kurt Hurst <kurthurst@Kurts-MacBook-Air.local>
2013-08-21 18:41:23 -0500

    added dynamic title attr

--- themes/BB_Custom/header.php
+++ themes/BB_Custom/header.php
@@ -6,7 +6,26 @@
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-        <title></title>
+        <title>
+            <?php
+            /*
+             * Print the <title> tag based on what is being viewed.
+             */
+            global $page, $paged;
+
+            wp_title( '|', true, 'right' );
+
+            // Add the blog name.
+            bloginfo( 'name' );
+
+            // Add the blog description for the home/front page.
+            $site_description = get_bloginfo( 'description', 'display' );
+            if ( $site_description && ( is_home() || is_front_page() ) )
+                echo " | $site_description";
+
+            // Add a page number if necessary:
+            if ( $paged >= 2 || $page >= 2 )
+                echo ' | ' . sprintf( __( 'Page %s', 'twentyeleven' ), max( $paged, $page ) ); ?></title>
         <meta name="description" content="">
         <meta content="width=device-width, initial-scale=1.0, minimum-scale=1.0" name="viewport">
         <style type="text/css">
